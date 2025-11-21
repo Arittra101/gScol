730f839c966f4c81498d8a580b98fe48c644a9cf
faisal ahammed <faisalahammed125@gmail.com>
2025-11-21 15:14:01 +0600

    feat(auth): Added login screen UI with Google and phone number options

---
+++ composeApp/src/androidMain/kotlin/org/getscol/gscol/Previews.kt
@@ -0,0 +1,11 @@
+package org.getscol.gscol
+
+import androidx.compose.runtime.Composable
+import org.getscol.gscol.auth.presentation.login.LoginScreen
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
