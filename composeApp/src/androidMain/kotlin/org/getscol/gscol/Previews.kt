2ff99e7f608d3939f1a579705ae80e07187d4520
faisal ahammed <faisalahammed125@gmail.com>
2025-11-21 16:43:14 +0600

    feat(auth): Add verification screen UI

--- composeApp/src/androidMain/kotlin/org/getscol/gscol/Previews.kt
+++ composeApp/src/androidMain/kotlin/org/getscol/gscol/Previews.kt
@@ -2,10 +2,11 @@ package org.getscol.gscol
 
 import androidx.compose.runtime.Composable
 import org.getscol.gscol.auth.presentation.login.LoginScreen
+import org.getscol.gscol.auth.presentation.verfication.VerificationScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
 fun LoginScreenPreview() {
-    LoginScreen()
+    VerificationScreen()
 }
