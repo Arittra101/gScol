f334c75f97a02c38c4bfbe1bb408e1ffdcccb130
faisal ahammed <faisalahammed125@gmail.com>
2025-12-02 00:12:48 +0600

    feat: Initialize timber for android logging

--- composeApp/src/androidMain/kotlin/org/getscol/gscol/ScolApplication.kt
+++ composeApp/src/androidMain/kotlin/org/getscol/gscol/ScolApplication.kt
@@ -3,10 +3,12 @@ package org.getscol.gscol
 import android.app.Application
 import org.getscol.gscol.core.di.initKoin
 import org.koin.android.ext.koin.androidContext
+import timber.log.Timber
 
 class ScolApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
         initKoin {
             androidContext(this@ScolApplication)
         }
