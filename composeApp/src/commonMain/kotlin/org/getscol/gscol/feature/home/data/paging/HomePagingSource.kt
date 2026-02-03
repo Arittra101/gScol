97b02bd7b99d0f6743e33207e041f418020d8e83
Arittra <arittra.das@brainstation-23.com>
2026-02-04 05:05:16 +0600

    Feature Home - Home  - cursor bug solve

--- composeApp/src/commonMain/kotlin/org/getscol/gscol/feature/home/data/paging/HomePagingSource.kt
+++ composeApp/src/commonMain/kotlin/org/getscol/gscol/feature/home/data/paging/HomePagingSource.kt
@@ -36,7 +36,7 @@ class HomePagingSource(
                     LoadResult.Page(
                         data = courses.toCourses(),
                         prevKey = null,
-                        nextKey = nextCursor
+                        nextKey = if(cursor == nextCursor) null else cursor
                     )
                 }
 
@@ -50,41 +50,3 @@ class HomePagingSource(
         }
     }
 }
-
-/*
-override fun getRefreshKey(state: PagingState<Int, Course>): Int? {
-    val anchorPosition = state.anchorPosition ?: return null //visible last item
-    val closestPage = state.closestPageToPosition(anchorPosition)
-    return closestPage?.prevKey?.plus(1) ?: closestPage?.nextKey?.minus(1)
-}
-
-override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Course> {
-    return try {
-        val page = params.key ?: 1
-        delay(2000)
-        val result = homeApiService.getHomeData(page, params.loadSize, isUserLogin)
-        when (result) {
-            is Result.Success -> {
-                val courses = if (!isUserLogin) result.data.data?.allCourses.orEmpty()
-                else result.data.data?.eligible?.courses.orEmpty()
-
-                val hasNext = result.data.data?.pagination?.hasNext ?: false
-
-                LoadResult.Page(
-                    data = courses.toCourses(),
-                    prevKey = if (page == 1) null else page - 1,
-                    nextKey = if (hasNext) page+1 else null
-                )
-            }
-
-            is Result.Error -> {
-                LoadResult.Error(
-                    Exception(result.error.toString())
-                )
-            }
-        }
-
-    } catch (e: Exception) {
-        LoadResult.Error(e)
-    }
-}*/
