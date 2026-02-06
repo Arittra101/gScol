d7ba35b281426f4eccfd5ef7e600c280345c8a49
Arittra <arittra.das@brainstation-23.com>
2026-02-07 04:35:55 +0600

    Feature: Home Screen - ios bottom animation issue fix

--- composeApp/src/iosMain/kotlin/org/getscol/gscol/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/org/getscol/gscol/Platform.ios.kt
@@ -1,16 +1,31 @@
 package org.getscol.gscol
 
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-    override val transitionAnimationTime: Int = 700
+    override val transitionAnimationTime: Int = 500
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
-actual fun platformEnterTransition() = EnterTransition.None
-actual fun platformExitTransition() = ExitTransition.None
-actual fun platformPopEnterTransition() = EnterTransition.None
-actual fun platformPopExitTransition() = ExitTransition.None
+val transitionAnimationTime = getPlatform().transitionAnimationTime
+
+actual fun platformEnterTransition() = fadeIn(
+    animationSpec = tween(durationMillis = transitionAnimationTime, easing = FastOutSlowInEasing)
+)
+
+actual fun platformExitTransition() = fadeOut(
+    animationSpec = tween(durationMillis = transitionAnimationTime, easing = FastOutSlowInEasing)
+)
+
+actual fun platformPopEnterTransition() = fadeIn(
+    animationSpec = tween(durationMillis = transitionAnimationTime, easing = FastOutSlowInEasing)
+)
+
+actual fun platformPopExitTransition() = fadeOut(
+    animationSpec = tween(durationMillis = transitionAnimationTime, easing = FastOutSlowInEasing)
+)
