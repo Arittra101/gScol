6e14304ee860788d4c040791921080965c6233d7
Arittra <arittra.das@brainstation-23.com>
2026-04-06 02:57:19 +0600

    Feature: Search Result - in Search result course adopt - favourite icon call back implement

--- composeApp/src/commonMain/kotlin/org/getscol/gscol/feature/search/presentation/search_result/SearchResultsViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/getscol/gscol/feature/search/presentation/search_result/SearchResultsViewModel.kt
@@ -3,18 +3,22 @@ package org.getscol.gscol.feature.search.presentation.search_result
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
+import androidx.paging.map
 import app.cash.paging.PagingData
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.update
 import org.getscol.gscol.core.data.session.Session
 import org.getscol.gscol.feature.home.domain.model.Course
+import org.getscol.gscol.feature.home.presentation.HomeAction
 import org.getscol.gscol.feature.search.domain.model.AdvancedSearchParams
 import org.getscol.gscol.feature.search.domain.repository.SearchRepository
+import kotlin.collections.plus
 
 data class SearchResultsParams(
     val searchText: String,
@@ -29,6 +33,7 @@ class SearchResultsViewModel(
 ) : ViewModel() {
 
     private val params = MutableStateFlow<SearchResultsParams?>(null)
+    private val favoriteUpdates = MutableStateFlow<Map<String, Boolean>>(emptyMap())
 
     fun setParams(searchText: String, listType: String, advancedParams: AdvancedSearchParams?) {
         params.update {
@@ -37,9 +42,8 @@ class SearchResultsViewModel(
         }
     }
 
-    val courses: Flow<PagingData<Course>> =
-        params
-            .flatMapLatest { p ->
+
+    val baseCourses: Flow<PagingData<Course>> = params.flatMapLatest { p ->
                 if (p == null) flow<PagingData<Course>> { }
                 else session.isUserLoggedIn
                     .distinctUntilChanged()
@@ -51,6 +55,20 @@ class SearchResultsViewModel(
                             advancedParams = p.advancedParams
                         )
                     }
+            }.cachedIn(viewModelScope)
+
+    val courses: Flow<PagingData<Course>> =
+        combine(baseCourses, favoriteUpdates) { pagingData, favourite ->
+            pagingData.map { course ->
+                favourite[course.courseId]?.let { newValue ->
+                    course.copy(isWishlisted = newValue)
+                } ?: course
             }
-            .cachedIn(viewModelScope)
+        }
+
+    fun onAction(action: HomeAction) {
+        when(action){
+            is HomeAction.AddToWishlist -> favoriteUpdates.update { current -> current + ((action.courseId to !action.isWishListed)) }
+        }
+    }
 }
