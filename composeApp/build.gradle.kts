b05a3688c1dec15f00bd620d6df5340cfdfe0c3a
Arittra <arittra.das@brainstation-23.com>
2026-06-03 18:08:21 +0600

    Feature: prevent auto android auto backup

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,7 +4,7 @@ import java.util.Properties
 
 val major = 0   // 1 to 99 ~ for big redesigns or breaking changes
 val minor = 9   // 0 to 99 ~ for new features
-val hotfix = 0  // 0 to 99 ~ for bug fixes only
+val hotfix = 1  // 0 to 99 ~ for bug fixes only
 
 fun generateVersionCode(): Int {
     val versionCode = major * 100000 + minor * 1000 + hotfix
